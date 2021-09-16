e656e918edf2605463e60e2bcf46e334714bfa2c
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-09-16 11:08:15 -0600

    Add tokenable module

--- lib/modules/tokenable.rb
+++ lib/modules/tokenable.rb
@@ -1,7 +1,7 @@
 require 'net/http'
 
 module Tokenable
-  def artsy_token
+  def self.artsy_token
     client_id = ENV['client_id']
     client_secret = ENV['client_secret']
     api_url = URI.parse('https://api.artsy.net/api/tokens/xapp_token')
