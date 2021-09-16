7e7c3d243c3e56fef434832606037403a6f9faf6
deebot10 <dee1jhill@gmail.com>
2021-09-16 16:03:12 -0600

    Update to download_image method

--- app/facades/get_art_facade.rb
+++ app/facades/get_art_facade.rb
@@ -9,7 +9,33 @@ class GetArtFacade
           info[x[:id]] = image
         end
       end
+      @info_values = info.values
+      @info = info
       info
     end
+
+    def jpeg_to_base64(info)
+      new_hash = {}
+
+      info.each do |key, value|
+        new_hash[key] = Base64.strict_encode64(value)
+      end
+      new_hash
+    end
+    
+    def download_image(url)
+      url = @info_values[0]
+      open(url) do |u|
+        File.open('jpg_to_convert.jpg', 'wb') { |f| f.write(u.read) }
+        base_64_img = File.open('jpg_to_convert.jpg', "rb") do |file|
+          Base64.strict_encode64(file.read)
+        end
+        
+        # require 'pry'; binding.pry
+        # here, you can make a call to the service with the base_64_img
+      end
+    end
+
+    # urls.each { |url| download_image(url) }  
   end
 end
--- app/services/cloud_vision_service.rb
+++ app/services/cloud_vision_service.rb
@@ -1,5 +1,18 @@
 class CloudVisionService
   class << self
-    
+    def artwork(download)
+      response = conn.post("/v1/images:annotate")
+      parse_json(response)
+    end
+
+    def conn
+      Faraday.new(url: 'https://vision.googleapis.com') do |req|
+        req.headers['']
+      end
+    end
+
+    def parse_json(response)
+      JSON.parse(response.body, symbolize_names: true)      
+    end
   end
 end
