7cb0aa3e16e2eb4e4f64417caeaee66cc0a4321e
Jacob Piland <piland.jacob@gmail.com>
2021-09-18 11:06:44 -0500

    Add show method and refactor create method for users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,15 +4,13 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def show
+    user = User.find(params[:id])
+    render json: UserSerializer.new(user)
   end
 
   def create
-    # if User.find_by(email: params[:email]) != nil
-    #   render json: {error: "Account already created.", status: :bad_request}
-    # else
-      user = User.create!(user_params)
-      render json: UserSerializer.new(user)
-    # end
+    user = User.create!(user_params)
+    render json: UserSerializer.new(user)
   end
 
   def destroy
