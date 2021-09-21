212292d117079f04ffb4512e4050ef534d85cd72
Jacob Piland <piland.jacob@gmail.com>
2021-09-21 16:26:00 -0500

    Add ArtCategory.create to art_categories method

--- app/facades/get_art_facade.rb
+++ app/facades/get_art_facade.rb
@@ -27,11 +27,18 @@ class GetArtFacade
           Category.find_or_create_by(name: description[:description])
         end
       end
+      category.each do |artsy_id, value|
+        value[:responses][0][:labelAnnotations].each do |description|
+          ArtCategory.create(art_id: (Art.find_by(artsy_id: artsy_id).id), category_id: (Category.find_by(name: description[:description]).id))
+        end
+      end
+
       category
+      # require "pry"; binding.pry
       # Hash Created of clound vision categories. Next step how to iterate through and group by quadrant.
-      
+
     #  art_category = category.each do |key, value|
-    #   hash = {} 
+    #   hash = {}
     #     category.values.each do |value2|
     #       value[:responses][0][:labelAnnotations].each do |description|
     #         hash[key].push(description[:description])
@@ -41,7 +48,7 @@ class GetArtFacade
     #   art_category
     #   require 'pry'; binding.pry
     end
-    
+
     def art_color(number, size)
       art = show_me_art(number, size)
       color = {}
