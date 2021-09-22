a161ca83e4391b9bd7309e79b94eece4c770c63b
Jacob Piland <piland.jacob@gmail.com>
2021-09-22 13:02:39 -0500

    Remove pry from spec

--- spec/requests/api/v1/art_category_request.rb
+++ spec/requests/api/v1/art_category_request.rb
@@ -4,11 +4,10 @@ RSpec.describe 'ArtCategory' do
   before :each do
     art = create(:art)
     category = create(:category)
-    post "/api/v1/art_categories", params: {art_id: "#{art.id}", category_id: "#{category.id}"} #, category: "#{category.name}"}
+    post "/api/v1/art_categories", params: {art_id: "#{art.id}", category_id: "#{category.id}"} 
   end
 
   it 'can create on the art_category table' do
-    require "pry"; binding.pry
     expect(ArtCategory.count).to eq(1)
   end
 end
