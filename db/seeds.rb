45122502c8624e8bdcf06bd731cd2558b1a5aba8
Jacob Piland <piland.jacob@gmail.com>
2021-09-23 15:47:45 -0500

    Remove rated_art from seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,32 +23,32 @@ end
 GetArtFacade.art_categories(50, 'larger')
 
 # create rated art
-user1 = User.first
-RatedArt.create!(user_id: user1.id, art_id: Art.first.id, liked: true)
-RatedArt.create!(user_id: user1.id, art_id: Art.all[1].id, liked: true)
-RatedArt.create!(user_id: user1.id, art_id: Art.all[2].id, liked: true)
-RatedArt.create!(user_id: user1.id, art_id: Art.all[3].id, liked: true)
-RatedArt.create!(user_id: user1.id, art_id: Art.all[4].id, liked: true)
-RatedArt.create!(user_id: user1.id, art_id: Art.all[5].id, liked: false)
-RatedArt.create!(user_id: user1.id, art_id: Art.all[6].id, liked: false)
-RatedArt.create!(user_id: user1.id, art_id: Art.all[7].id, liked: false)
-
-user2 = User.all[1]
-RatedArt.create!(user_id: user2.id, art_id: Art.first.id, liked: true)
-RatedArt.create!(user_id: user2.id, art_id: Art.all[1].id, liked: true)
-RatedArt.create!(user_id: user2.id, art_id: Art.all[2].id, liked: true)
-RatedArt.create!(user_id: user2.id, art_id: Art.all[3].id, liked: true)
-RatedArt.create!(user_id: user2.id, art_id: Art.all[4].id, liked: true)
-RatedArt.create!(user_id: user2.id, art_id: Art.all[5].id, liked: true)
-RatedArt.create!(user_id: user2.id, art_id: Art.all[6].id, liked: true)
-RatedArt.create!(user_id: user2.id, art_id: Art.all[7].id, liked: false)
-
-user3 = User.all[2]
-RatedArt.create!(user_id: user3.id, art_id: Art.first.id, liked: false)
-RatedArt.create!(user_id: user3.id, art_id: Art.all[1].id, liked: true)
-RatedArt.create!(user_id: user3.id, art_id: Art.all[2].id, liked: false)
-RatedArt.create!(user_id: user3.id, art_id: Art.all[3].id, liked: true)
-RatedArt.create!(user_id: user3.id, art_id: Art.all[4].id, liked: false)
-RatedArt.create!(user_id: user3.id, art_id: Art.all[5].id, liked: true)
-RatedArt.create!(user_id: user3.id, art_id: Art.all[6].id, liked: true)
-RatedArt.create!(user_id: user3.id, art_id: Art.all[7].id, liked: false)
+# user1 = User.first
+# RatedArt.create!(user_id: user1.id, art_id: Art.first.id, liked: true)
+# RatedArt.create!(user_id: user1.id, art_id: Art.all[1].id, liked: true)
+# RatedArt.create!(user_id: user1.id, art_id: Art.all[2].id, liked: true)
+# RatedArt.create!(user_id: user1.id, art_id: Art.all[3].id, liked: true)
+# RatedArt.create!(user_id: user1.id, art_id: Art.all[4].id, liked: true)
+# RatedArt.create!(user_id: user1.id, art_id: Art.all[5].id, liked: false)
+# RatedArt.create!(user_id: user1.id, art_id: Art.all[6].id, liked: false)
+# RatedArt.create!(user_id: user1.id, art_id: Art.all[7].id, liked: false)
+#
+# user2 = User.all[1]
+# RatedArt.create!(user_id: user2.id, art_id: Art.first.id, liked: true)
+# RatedArt.create!(user_id: user2.id, art_id: Art.all[1].id, liked: true)
+# RatedArt.create!(user_id: user2.id, art_id: Art.all[2].id, liked: true)
+# RatedArt.create!(user_id: user2.id, art_id: Art.all[3].id, liked: true)
+# RatedArt.create!(user_id: user2.id, art_id: Art.all[4].id, liked: true)
+# RatedArt.create!(user_id: user2.id, art_id: Art.all[5].id, liked: true)
+# RatedArt.create!(user_id: user2.id, art_id: Art.all[6].id, liked: true)
+# RatedArt.create!(user_id: user2.id, art_id: Art.all[7].id, liked: false)
+#
+# user3 = User.all[2]
+# RatedArt.create!(user_id: user3.id, art_id: Art.first.id, liked: false)
+# RatedArt.create!(user_id: user3.id, art_id: Art.all[1].id, liked: true)
+# RatedArt.create!(user_id: user3.id, art_id: Art.all[2].id, liked: false)
+# RatedArt.create!(user_id: user3.id, art_id: Art.all[3].id, liked: true)
+# RatedArt.create!(user_id: user3.id, art_id: Art.all[4].id, liked: false)
+# RatedArt.create!(user_id: user3.id, art_id: Art.all[5].id, liked: true)
+# RatedArt.create!(user_id: user3.id, art_id: Art.all[6].id, liked: true)
+# RatedArt.create!(user_id: user3.id, art_id: Art.all[7].id, liked: false)
