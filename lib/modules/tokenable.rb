32079ead870f710efc63d71a8bd73d1882d395a7
Jacob Piland <piland.jacob@gmail.com>
2021-09-22 19:58:44 -0500

    Add 3 day cache to token module

--- lib/modules/tokenable.rb
+++ lib/modules/tokenable.rb
@@ -4,8 +4,11 @@ module Tokenable
   def artsy_token
     client_id = ENV['client_id']
     client_secret = ENV['client_secret']
-    api_url = URI.parse('https://api.artsy.net/api/tokens/xapp_token')
-    response = Net::HTTP.post_form(api_url, client_id: client_id, client_secret: client_secret)
-    xapp_token = JSON.parse(response.body)['token']
+    if Rails.cache.fetch('token') == nil
+      api_url = URI.parse('https://api.artsy.net/api/tokens/xapp_token')
+      response = Net::HTTP.post_form(api_url, client_id: client_id, client_secret: client_secret)
+      Rails.cache.write('token', JSON.parse(response.body)['token'], expires_in: 3.days)
+    end
+    xapp_token = Rails.cache.fetch('token')
   end
 end
