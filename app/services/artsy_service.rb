4d745922254a7387b265548e8f63bc1984660f02
Alex Klick <aklick12@gmail.com>
2021-09-22 21:49:02 -0600

    rails cache

--- app/services/artsy_service.rb
+++ app/services/artsy_service.rb
@@ -4,22 +4,28 @@ class ArtsyService
   extend Tokenable
 
   class << self
-    def find_art_by_id(id) #For finding art by artsy_id
-      response = conn.get("/api/artworks/#{id}")
-      parse_json(response)
+    def find_art_by_id(artsy_id) #For finding art by artsy_id
+      cache = Rails.cache.fetch(artsy_id)
+      if cache == nil 
+        response = conn.get("/api/artworks/#{artsy_id}")
+        formatted_res = JSON.parse(response.body, symbolize_names: true)
+        Rails.cache.write(formatted_res[:id], formatted_res)
+      else
+        cache
+      end
     end
 
     def conn
-      binding.pry
-      token = Tokenable.artsy_token
+      token = artsy_token
       Faraday.new(url: 'https://api.artsy.net') do |req|
         req.headers['X-Xapp-Token'] = "#{token}"
       end
     end
 
     def parse_json(response)
-      JSON.parse(response.body, symbolize_names: true)
-      binding.pry
+      formatted_res = JSON.parse(response.body, symbolize_names: true)
+      cache = Rails.cache.fetch(formatted_res[:id])
     end
   end
 end
+
